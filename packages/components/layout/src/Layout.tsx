92f3fb2bf631648c86eef7a02af108ff5b10d4e7
Guido Genzone <ggenzone@protonmail.com>
2023-08-29 12:02:43 +0200

    feat(components/layout): Add wrapper

--- packages/components/layout/src/Layout.tsx
+++ packages/components/layout/src/Layout.tsx
@@ -2,10 +2,21 @@ import * as React from "react"
 
 export interface LayoutProps {
   children: React.ReactNode
+  className?: string
 }
 
-export function Layout(props: LayoutProps) {
-  return <button>{props.children}</button>
+export function Layout (props: LayoutProps): JSX.Element {
+  return (
+    <div className={`h-screen overflow-hidden w-full ${props.className ?? ''}`}>
+      <div className="h-screen overflow-hidden relative">
+        <div className="flex items-start">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  )
 }
+
+
 
 Layout.displayName = "Layout"
